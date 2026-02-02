51fbe4a7818cb47f367e399ae181c4ea6bb52953
GitHub User <user@github.com>
2026-02-02 18:05:04 +0300

    Navigation fix: Ensure pages start at the top on transition

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+import ScrollToTop from './ScrollToTop'
 
 function Layout() {
     return (
         <>
+            <ScrollToTop />
             <Header />
             <main>
                 <Outlet />
