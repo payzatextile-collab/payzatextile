75fcca8e5a786b8b766819e97e464f9290815870
GitHub User <user@github.com>
2026-02-02 15:50:51 +0300

    Fix: Remove basename from Router for Vercel root deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <BrowserRouter basename="/t/payzatextile">
+        <BrowserRouter>
             <App />
         </BrowserRouter>
     </React.StrictMode>,
